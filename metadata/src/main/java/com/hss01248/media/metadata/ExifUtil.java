ef2f32b4be8460a654689ec1159b4e797ea48efc
hss01248 <hss012489@gmail.com>
2021-03-25 16:09:17 +0800

    优化信息显示

--- metadata/src/main/java/com/hss01248/media/metadata/ExifUtil.java
+++ metadata/src/main/java/com/hss01248/media/metadata/ExifUtil.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import androidx.exifinterface.media.ExifInterface;
 
+import android.graphics.ColorSpace;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.text.TextUtils;
@@ -53,6 +54,7 @@ public class ExifUtil {
                             String tag = (String) field.get(ExifInterface.class);
                             String val = exif.getAttribute(tag);
                             if(!TextUtils.isEmpty(val)){
+                               val =  stringfySomeTag(tag,val);
                                 exifMap.put(tag,val);
                             }
                         }else {
@@ -78,6 +80,78 @@ public class ExifUtil {
             return exifMap;
         }
     }
+
+    private static String stringfySomeTag(String tag, String val) {
+        if(ExifInterface.TAG_GPS_LATITUDE.equals(tag) || ExifInterface.TAG_GPS_LONGITUDE.equals(tag)){
+            try {
+                return parseGps(val);
+            }catch (Throwable throwable){
+                throwable.printStackTrace();
+                return val;
+            }
+
+        }
+        if(ExifInterface.TAG_COLOR_SPACE.equals(tag)){
+            return parseColorSpace(val);
+        }
+        if(ExifInterface.TAG_ORIENTATION.equals(tag)){
+            return parseOritation(val);
+        }
+        return val;
+    }
+
+    private static String parseOritation(String val) {
+        if((ExifInterface.COLOR_SPACE_S_RGB+"").equals(val)){
+            return "sRGB";
+        }else if((ExifInterface.COLOR_SPACE_UNCALIBRATED+"").equals(val)){
+            return "UNCALIBRATED";
+        }
+        return "sRGB";
+    }
+
+    private static String parseColorSpace(String val) {
+       if((ExifInterface.ORIENTATION_ROTATE_90+"").equals(val)){
+           return "90";
+        }else if((ExifInterface.ORIENTATION_ROTATE_180+"").equals(val)){
+           return "180";
+       }else if((ExifInterface.ORIENTATION_ROTATE_270+"").equals(val)){
+           return "270";
+       }
+        return "0";
+    }
+
+    private static String parseGps(String val) {
+        val = val.replace("/", ",");
+        String[] lat = val.split(",");
+        Float latD =0f;
+
+        Float latM =0f;
+
+        Float latS =0f;
+        if (lat.length >=2) {
+
+            latD = Float.parseFloat(lat[0]) / Float.parseFloat(lat[1]);
+
+        }
+
+        if (lat.length >=4) {
+
+            latM = Float.parseFloat(lat[2]) / Float.parseFloat(lat[3]);
+
+        }
+
+        if (lat.length >=6) {
+
+            latS = Float.parseFloat(lat[4]) / Float.parseFloat(lat[5]);
+
+        }
+
+
+
+       float latitude = latD + latM /60 + latS /3600;
+        return latitude+"";
+    }
+
 
     private static void json(Map<String, String> exifMap) {
        Log.i("exif", exifMap.toString());
